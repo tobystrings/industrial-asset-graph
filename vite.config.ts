71f129a55413d65797b33e3367b7bda5f42974a4
Toby Strings <tobystrings@gmail.com>
2026-07-20 11:52:41 -0700

    Replace vite.config.ts from working ZIP

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,4 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/industrial-asset-graph/', // This matches your exact repository name
-});
+export default defineConfig({ plugins: [react()] });
