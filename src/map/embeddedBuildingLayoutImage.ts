ebca4f406162ca66b53cb61848b661740039d269
Toby Strings <tobystrings@gmail.com>
2026-08-19 15:38:16 -0700

    Fix embedded 2D map image data

--- src/map/embeddedBuildingLayoutImage.ts
+++ src/map/embeddedBuildingLayoutImage.ts
@@ -1,13 +1,33 @@
 import chunk1 from './buildingLayoutImageChunks/chunk-01';
-import chunk2 from './buildingLayoutImageChunks/chunk-02';
-import chunk3 from './buildingLayoutImageChunks/chunk-03';
+import chunk2a from './buildingLayoutImageChunks/chunk-02-1';
+import chunk2b from './buildingLayoutImageChunks/chunk-02-2';
+import chunk2c from './buildingLayoutImageChunks/chunk-02-3';
+import chunk3a from './buildingLayoutImageChunks/chunk-03-1';
+import chunk3b from './buildingLayoutImageChunks/chunk-03-2';
+import chunk3c from './buildingLayoutImageChunks/chunk-03-3';
 import chunk4 from './buildingLayoutImageChunks/chunk-04';
 import chunk5 from './buildingLayoutImageChunks/chunk-05';
 import chunk6 from './buildingLayoutImageChunks/chunk-06';
-import chunk7 from './buildingLayoutImageChunks/chunk-07';
-import chunk8 from './buildingLayoutImageChunks/chunk-08';
-import chunk9 from './buildingLayoutImageChunks/chunk-09';
+import chunk7a from './buildingLayoutImageChunks/chunk-07-1';
+import chunk7b from './buildingLayoutImageChunks/chunk-07-2';
+import chunk7c from './buildingLayoutImageChunks/chunk-07-3';
+import chunk8a from './buildingLayoutImageChunks/chunk-08-1';
+import chunk8b from './buildingLayoutImageChunks/chunk-08-2';
+import chunk8c from './buildingLayoutImageChunks/chunk-08-3';
+import chunk9a from './buildingLayoutImageChunks/chunk-09-1';
+import chunk9b from './buildingLayoutImageChunks/chunk-09-2';
+import chunk9c from './buildingLayoutImageChunks/chunk-09-3';
 
-const buildingLayoutImage = `data:image/webp;base64,${chunk1 + chunk2 + chunk3 + chunk4 + chunk5 + chunk6 + chunk7 + chunk8 + chunk9}`;
+const buildingLayoutImage =
+  'data:image/webp;base64,' +
+  chunk1 +
+  chunk2a + chunk2b + chunk2c +
+  chunk3a + chunk3b + chunk3c +
+  chunk4 +
+  chunk5 +
+  chunk6 +
+  chunk7a + chunk7b + chunk7c +
+  chunk8a + chunk8b + chunk8c +
+  chunk9a + chunk9b + chunk9c;
 
 export default buildingLayoutImage;
