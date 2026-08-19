9fa58007270e677afee3ff1e0c433412b19a633e
Toby Strings <tobystrings@gmail.com>
2026-08-19 12:13:20 -0700

    apply shared visual system

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './dashboard.css';
+import './ui/system.css';
 import './motion/tokens.css';
 import { GuideProvider } from './features/facility-guide';
 import { FacilityProvider } from './facility';
