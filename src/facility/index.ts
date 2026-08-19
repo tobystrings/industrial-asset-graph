ada3359419834a91686948f2b363d9e30cbca988
Toby Strings <tobystrings@gmail.com>
2026-08-19 16:49:26 -0700

    feat: export facility editor runtime API

--- src/facility/index.ts
+++ src/facility/index.ts
@@ -1,3 +1,5 @@
 export { activeFacilityPackage, liebFacilityPackage, selectFacilityPackage } from './activeFacility';
-export { FacilityProvider, useFacility } from './FacilityProvider';
+export { FacilityProvider, useFacility, useFacilityEditor } from './FacilityProvider';
+export type { FacilityEditorApi } from './FacilityProvider';
+export type { AttachmentRecord, ObservationRecord, PlantBackup } from './runtimeDb';
 export type { AssetSerialSource, FacilityFeatureConfig, FacilityIdentity, FacilityPackage } from './types';
