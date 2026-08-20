4db173acb8e975f0b6df672bd7be5626cf56c379
Toby Strings <tobystrings@gmail.com>
2026-08-19 17:05:28 -0700

    Export plant editor map and archive types

--- src/facility/index.ts
+++ src/facility/index.ts
@@ -1,5 +1,5 @@
 export { activeFacilityPackage, liebFacilityPackage, selectFacilityPackage } from './activeFacility';
 export { FacilityProvider, useFacility, useFacilityEditor } from './FacilityProvider';
 export type { FacilityEditorApi } from './FacilityProvider';
-export type { AttachmentRecord, ObservationRecord, PlantBackup } from './runtimeDb';
-export type { AssetSerialSource, FacilityFeatureConfig, FacilityIdentity, FacilityPackage } from './types';
+export type { AttachmentRecord, IagArchiveManifest, ObservationRecord, PlantBackup } from './runtimeDb';
+export type { AssetSerialSource, FacilityFeatureConfig, FacilityIdentity, FacilityMapMarker, FacilityMapMarkerState, FacilityMapMarkerTone, FacilityPackage } from './types';
